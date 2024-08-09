f1b09c39d45f66659d834d4adaed1f27e1ba528c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-09 04:28:09 +0200

    created the tables

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -76,15 +76,12 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemonBackend',
         'USER': 'root',
         'PASSWORD': 'Alpy,1986',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-         }
  }
 }
 
